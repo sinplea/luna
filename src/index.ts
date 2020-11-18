91874381b16ffaf9979071b2be0403406f22747d
Alex Werner <alexanderdwerner@gmail.com>
2020-11-18 16:13:40 -0600

    wip: creates handleCreated and handleJoin as a start to room management with redis

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,66 @@
 import express from 'express';
 import http from 'http';
-import { createRoomManager } from './lib/RoomManger';
+import redis, { RedisClient } from 'redis';
+import { handleCreated, handleJoin } from './lib/RoomManger';
+import type { RoomDetails } from './lib/types/RoomDetails';
 
-const app: Express.Application = express();
+const SOCKET_OPTIONS: unknown = {
+  cors: true,
+  origins:["http://127.0.0.1:5000"],
+};
+const PORT: number = 8080;
+
+const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port: number = 8080;
+const io: SocketIO.Server = require('socket.io')(server, SOCKET_OPTIONS);
+const client: RedisClient = redis.createClient();
 
-server.listen(port, () => {
-  createRoomManager(server);
+// SETUP REDIS
+client.on('error', (err): void => {
+  console.log(`Redis error: ${err}`);
+})
+
+// HANDLE SOCKETS
+io.on('connection', (socket: SocketIO.Socket) => {
+  let room: string = "";
+
+  console.log(`Socket connected: ${socket.id}`)
+
+  // 1. Room Management
+
+  socket.on('room-created', (details: RoomDetails) => {
+    room = handleCreated(client, details);
+    socket.join(room);
+    socket.emit('new-room-created', { room })
+  })
+
+  socket.on('room-joined', (roomId: string) => {
+    const success = handleJoin(client, roomId, socket.id);
+
+    if (success) {
+      socket.join(roomId);
+      socket.emit('room-join-succeeded')
+    } else {
+      socket.emit('room-join-failed')
+    }
+  })
+
+  // 2. Chat Room Usage
+
+  socket.on('chat-message-posted', (message) => {
+    if (room) {
+      console.log(`Socket: ${socket.id} wrote message: ${message.body} to room ${room}`)
+      socket.to(room).emit('message-captured', message);
+    }
+  })
+
+  socket.on('disconnect', (reason: string) => {
+
+  })
+})
+
+// SERVER INITIALIZATION
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
 });
--- src/lib/RoomManger.ts
+++ src/lib/RoomManger.ts
@@ -2,11 +2,41 @@ import { nanoid } from 'nanoid';
 import { RedisClient } from 'redis';
 import type { RoomDetails } from './types/RoomDetails';
 
-const ROOM_SOCKETS_SET_NAME: string = 'watch-party:room_sockets';
+const ROOM_POPULATION_SET_NAME: string = 'watch-party:room_population';
 const PUBLIC_ROOMS_ZSET_NAME: string = 'watch-party:public_rooms';
 const ROOM_DETAILS_HASH_NAME: string = 'watch-party:room_details';
 
-export function handleNewRoomCreated(client: RedisClient, details: RoomDetails): string {
-  const id = nanoid(12);
-  return id;
+export function handleCreated(client: RedisClient, details: RoomDetails): string {
+  const roomId = nanoid(12);
+
+  const hashmapFields = [
+    "isPublic", details.isPublic,
+    "currentlyWatching", details.currentlyWatching,
+    "previewThumbnail", details.previewThumbnail,
+    "leader", details.leader,
+  ]
+
+  client.sadd(`${ROOM_POPULATION_SET_NAME}:${roomId}`, details.leader);
+  client.zadd(`${PUBLIC_ROOMS_ZSET_NAME}`, 1, roomId);
+  client.hmset(`${ROOM_DETAILS_HASH_NAME}:${roomId}`, hashmapFields)
+
+  return roomId;
+}
+
+export function handleJoin(client: RedisClient, roomId: string, socketId: string): boolean {
+  const exists = client.exists(`${ROOM_POPULATION_SET_NAME}:${roomId}`)
+
+  if (exists) {
+    const isPublic = client.hmget(`${ROOM_DETAILS_HASH_NAME}:${roomId}`, "isPublic");
+
+    if (isPublic) {
+      client.sadd(`${ROOM_POPULATION_SET_NAME}:${roomId}`, socketId);
+      client.zincrby(`${PUBLIC_ROOMS_ZSET_NAME}`, 1, roomId);
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
